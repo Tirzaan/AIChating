0595a18bb123f494e40a1dab93db9e5480e0099f
Tirzaan Neil <Tirzaan@tntneil.com>
2026-02-04 21:19:34 -0700

    Initial Commit

---
+++ AIChating/AIChatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatingApp.swift
+//  AIChating
+//
+//  Created by Tirzaan on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatingTests/AIChatingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatingTests.swift
+//  AIChatingTests
+//
+//  Created by Tirzaan on 2/4/26.
+//
+
+import Testing
+@testable import AIChating
+
+struct AIChatingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
