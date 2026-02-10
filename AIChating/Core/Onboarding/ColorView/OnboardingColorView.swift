6fe6e473f690f1869a40c3d818765e0a7c8f0b98
Tirzaan Neil <Tirzaan@tntneil.com>
2026-02-10 16:29:16 -0700

    [Feature] OnboardingIntroView, OnboardingColorView

---
+++ AIChating/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,79 @@
+//
+//  OnboardingColorView.swift
+//  AIChating
+//
+//  Created by Tirzaan on 2/10/26.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    @State private var selectedColor: Color?
+    let profileColors: [Color] = [.red, .green, .orange, .blue, .mint, .purple, .cyan, .teal, .indigo]
+    
+    var body: some View {
+        ScrollView {
+            colorGrid
+            .padding(.horizontal, 24)
+        }
+        .safeAreaInset(
+            edge: .bottom,
+            alignment: .center,
+            spacing: 16,
+            content: {
+                ZStack {
+                    if let selectedColor {
+                        continueButton
+                        .transition(AnyTransition(.move(edge: .bottom)))
+                    }
+                }
+                .padding(24)
+                .background(selectedColor != nil ? Color(UIColor.systemBackground) : .clear)
+            })
+        .animation(.spring, value: selectedColor)
+    }
+    
+    private var colorGrid: some View {
+        LazyVGrid(
+            columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+            alignment: .center,
+            spacing: 16,
+            pinnedViews: [.sectionHeaders],
+            content: {
+                Section {
+                    ForEach(profileColors, id: \.self) { color in
+                        Circle()
+                            .fill(.accent)
+                            .overlay {
+                                color
+                                    .clipShape(Circle())
+                                    .padding(selectedColor == color ? 10 : 0)
+                            }
+                            .onTapGesture {
+                                selectedColor = color
+                            }
+                    }
+                } header: {
+                    Text("Select A Profile Color")
+                        .font(.headline)
+                }
+                
+            }
+        )
+    }
+    
+    private var continueButton: some View {
+        NavigationLink {
+            OnboardingCompletedView()
+        } label: {
+            Text("Continue")
+                .callToActionButton()
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingColorView()
+    }
+}
---
+++ AIChating/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingIntroView.swift
+//  AIChating
+//
+//  Created by Tirzaan on 2/10/26.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        VStack {
+            Text("Make your own \(Text("Avatars").foregroundStyle(.accent).fontWeight(.semibold)) and chat with them!\n\nHave \(Text("real conversations").foregroundStyle(.accent).fontWeight(.semibold)) with AI generated responses!")
+                .baselineOffset(6)
+                .frame(maxHeight: .infinity)
+                .padding(24)
+            
+            NavigationLink {
+                OnboardingColorView()
+            } label: {
+                Text("Continue")
+                    .callToActionButton()
+            }
+        }
+        .padding(24)
+        .font(.title2)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingIntroView()
+    }
+}
