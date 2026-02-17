04041bf7a24892861a5d3493192cf82c54cbbc9c
Tirzaan Neil <Tirzaan@tntneil.com>
2026-02-17 15:08:19 -0700

    [Feature] CarouselCell

---
+++ AIChating/Components/Views/CarouselCell.swift
@@ -0,0 +1,69 @@
+//
+//  CarouselCell.swift
+//  AIChating
+//
+//  Created by Tirzaan on 2/17/26.
+//
+
+import SwiftUI
+
+struct CarouselCell<Content: View, T: Hashable>: View {
+    var items: [T]
+    @ViewBuilder var content: (T) -> Content
+    @State private var selection: T?
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(items, id: \.self) { item in
+                        content(item)
+                            .scrollTransition(.interactive.threshold(.visible(0.95)),transition: { content, phase in
+                                content
+                                    .scaleEffect(phase.isIdentity ? 1 : 0.9)
+                            })
+                            .containerRelativeFrame(.horizontal, alignment: .center)
+                            .id(item)
+                    }
+                }
+            }
+            .frame(height: 200)
+            .scrollIndicators(.hidden)
+            .scrollTargetLayout()
+            .scrollTargetBehavior(.paging)
+            .scrollPosition(id: $selection)
+            .onChange(of: items.count, { _, _ in
+                updateSelectionIfNeeded()
+            })
+            .onAppear {
+                updateSelectionIfNeeded()
+            }
+            
+            HStack(spacing: 8) {
+                ForEach(items, id: \.self) { item in
+                    Circle()
+                        .fill(item == selection ? .accent : .secondary.opacity(0.5))
+                        .frame(width: 8, height: 8)
+                }
+            }
+            .animation(.linear, value: selection)
+        }
+    }
+    
+    private func updateSelectionIfNeeded() {
+        if selection == nil || selection == items.last {
+            selection = items.first
+        }
+    }
+}
+
+#Preview {
+    CarouselCell(items: AvatarModel.mocks) { item in
+        HeroCell(
+            title: item.name,
+            subtitle: item.description,
+            imageName: item.profileImageName
+        )
+    }
+        .padding()
+}
