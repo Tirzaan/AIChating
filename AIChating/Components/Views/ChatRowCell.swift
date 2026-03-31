ba16e9c8006665b38724ddd59645eb55dd7cf655
Tirzaan Neil <Tirzaan@tntneil.com>
2026-03-31 15:47:36 -0600

    [Feature] ChatRowCell, ChatRowCellViewBuilder

---
+++ AIChating/Components/Views/ChatRowCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRowCell.swift
+//  AIChating
+//
+//  Created by Tirzaan on 3/25/26.
+//
+
+import SwiftUI
+
+struct ChatRowCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatRowCell()
+}
---
+++ AIChating/Components/Views/ChatRowCellViewBuilder.swift
@@ -0,0 +1,18 @@
+//
+//  ChatRowCellViewBuilder.swift
+//  AIChating
+//
+//  Created by Tirzaan on 3/25/26.
+//
+
+import SwiftUI
+
+struct ChatRowCellViewBuilder: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatRowCellViewBuilder()
+}
